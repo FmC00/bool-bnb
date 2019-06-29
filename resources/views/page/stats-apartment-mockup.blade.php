639340eb60d005fb3749aa9ac381b28b7780f70a
FmC00 <michele.consoli00@gmail.com>
2019-06-29 21:50:02 +0200

    update

--- resources/views/page/stats-apartment-mockup.blade.php
+++ resources/views/page/stats-apartment-mockup.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.dashboard-layout')
-@section('title', 'Statistiche dell\'appartamento')
+@section('title', "Statistiche dell'appartamento")
 @section('right-options')
   {{-- right-options --}}
 @stop
 @section('content')
 <div class="w-100 d-flex">
   <h1>{{$apartment->name}}</h1>
-  <div>ciao</div>
+
 
 </div>
 
